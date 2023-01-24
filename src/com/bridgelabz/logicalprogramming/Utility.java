601e0827c693fee1243d90ebd7a56efc93af5f0c
amitatiwari255 <amita.tiwari255@gmail.com>
2023-01-24 08:54:34 +0530

    Added:Logical Programming Programs

---
+++ src/com/bridgelabz/logicalprogramming/Utility.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.logicalprogramming;
+import java.util.Scanner;
+public class Utility {
+    private static Scanner scan = new Scanner(System.in);
+    public static int getIntValue(){
+        return scan.nextInt();
+    }
+}
